eea9a7f5e6a31f5023a276d8d3725fef1c023811
Siddharth <siddharth.060605@gmail.com>
2025-05-07 13:25:08 -0500

    Add unadded large components

---
+++ src/lib/components/large/ai-media-input.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { Textarea } from "../ui/textarea";
+
+    let { type } = $props();
+    let value = $state("")
+</script>
+
+{#if type == "text"}
+    <Textarea placeholder="Enter your message" bind:value></Textarea>
+{:else if type == "audio"}{:else if type == "video"}{:else if type == "3D"}{/if}
---
+++ src/lib/components/large/single-select.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { Select } from "$lib/components/ui/select";
+    import SelectContent from "$lib/components/ui/select/select-content.svelte";
+    import SelectItem from "$lib/components/ui/select/select-item.svelte";
+    import SelectTrigger from "$lib/components/ui/select/select-trigger.svelte";
+
+    let { options, value = $bindable() } = $props();
+</script>
+
+<Select type="single" bind:value={value}>
+    <SelectTrigger placeholder="Heloeawf">
+        {value}
+    </SelectTrigger>
+    <SelectContent placeholder="Value">
+        {#each options as option}
+            <SelectItem value={option}></SelectItem>
+        {/each}
+    </SelectContent>
+</Select>
