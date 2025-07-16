ebe71eb35153ea1fb3c3b2d1207b004ecc258eb0
Siddharth <siddharth.060605@gmail.com>
2025-07-16 16:51:19 -0400

    Add triton api class

--- src/lib/api/fetcher.ts
+++ src/lib/api/fetcher.ts
@@ -1,5 +1,4 @@
 import { z } from "zod"
-import { modelInfoResponse } from "./triton/triton-inference-schema"
 
 export async function schemaFetch<T extends z.ZodTypeAny>(args: Parameters<typeof fetch>, schema: T): Promise<z.infer<T>> {
     let response = await fetch(args[0], args[1])
--- src/lib/api/triton/api.ts
+++ src/lib/api/triton/api.ts
@@ -1,13 +1,77 @@
+import type { z } from "zod";
 import { schemaFetch } from "../fetcher";
+import { modelIndexResponse, modelInfoResponse } from "./api-schema";
 
-class TritonApiInstance {
+export class TritonApiInstance {
     url: string;
+    ready: Promise<void>
 
-    constructor(url: string, options: any) {
-        this.url = url
+    models!: z.infer<typeof modelInfoResponse>[];
+
+    constructor(url: string, options?: any) {
+        this.url = url;
+
+        this.ready = this.fetchModels().then(async (models) => {
+            this.models = await Promise.all(models.map(async model => {
+                return { ...model, ...(await this.modelInfo(model.name)) }
+            }))
+        });
+
     }
 
-    async getModels() {
-        schemaFetch(`${url}/v2/repository/index`, { method: "POST"})
+    async fetchModels() {
+        return schemaFetch([`${this.url}/v2/repository/index`, { method: "POST" }], modelIndexResponse)
+    }
+
+    async filterModels(
+        inputType: string,
+        outputType: string
+    ) {
+        await this.ready
+        let filtered = this.models.filter(async (model) => {
+            let containsCorrectInput =
+                model.input.filter((input) => {
+                    input.data_type == inputType;
+                }).length > 0;
+
+            let containsCorrectOutput =
+                model.output.filter((output) => {
+                    output.data_type == outputType;
+                }).length > 0;
+
+            return (
+                containsCorrectInput && containsCorrectOutput && model.state == "READY"
+            );
+        });
+
+        return filtered
+    }
+
+    async modelInfo(model_name: string) {
+        return schemaFetch([`${this.url}/v2/models/${model_name}/config`], modelInfoResponse)
+    }
+
+    async infer(model_name: string, inputs: any) {
+        console.log(inputs)
+        return fetch(`${this.url}/${model_name}/infer`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                inputs: inputs,
+                outputs: [{ name: "encoder_hidden_states" }],
+            }),
+        });
     }
 }
+
+let tritonEndpoint = "https://test.novaphaze.com";
+export let triton = new TritonApiInstance(tritonEndpoint);
+
+// [
+//                     {
+//                         name: "input_ids",
+//                         shape: [1, input_ids.length],
+//                         datatype: "INT32",
+//                         // data: input_ids,
+//                     },
+//                 ],
