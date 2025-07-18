3bb4723036ec16fd37c1149655776b60a9153e02
Siddharth <siddharth.060605@gmail.com>
2025-07-17 22:48:28 -0400

    Significantly improve sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,60 +1,93 @@
 <script lang="ts">
   import "../app.css";
 
-  import {
-    Sidebar,
-    SidebarContent,
-    SidebarFooter,
-    SidebarGroup,
-    SidebarHeader,
-    SidebarMenu,
-    SidebarProvider,
-    SidebarTrigger,
-  } from "$lib/components/ui/sidebar";
   import SettingsIcon from "@lucide/svelte/icons/settings";
   import { ModeWatcher } from "mode-watcher";
   import ThemeSwitcher from "$lib/components/large/theme-switcher.svelte";
-  import SidebarMenuItem from "$lib/components/ui/sidebar/sidebar-menu-item.svelte";
-  import SidebarGroupLabel from "$lib/components/ui/sidebar/sidebar-group-label.svelte";
-  import SidebarGroupContent from "$lib/components/ui/sidebar/sidebar-group-content.svelte";
-  import SidebarMenuButton from "$lib/components/ui/sidebar/sidebar-menu-button.svelte";
+
+  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+  import ChevronDown from "@lucide/svelte/icons/chevron-down";
+  import SearchIcon from "@lucide/svelte/icons/search";
+
+  import { Collapsible } from "bits-ui";
 
   let { children } = $props();
 
-  let items = {
-	
-  }
+  let groups = [
+    {
+      title: "Settings",
+      icon: SettingsIcon,
+      collapsible: true,
+      items: [
+        {
+          title: "piece",
+          link: "#",
+          icon: SearchIcon,
+        },
+      ],
+    },
+  ];
 </script>
 
 <ModeWatcher />
 
 <div class="flex gap-4">
-  <SidebarProvider style="--sidebar-width: 20rem;">
-    <Sidebar>
-      <SidebarHeader></SidebarHeader>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <SidebarMenuButton
-                  ><SettingsIcon>
-
-				  </SettingsIcon>
-				  <a href="/settings">Settings</a></SidebarMenuButton
+  <Sidebar.Provider style="--sidebar-width: 20rem;">
+    <Sidebar.Root>
+      <Sidebar.Header></Sidebar.Header>
+      <Sidebar.Content>
+        {#each groups as group}
+          {#if group.collapsible}
+            <Collapsible.Root class="group/collapsible">
+              <Sidebar.Group>
+                <Sidebar.GroupLabel
+                  class="hover:bg-sidebar-accent hover:text-sidebar-accent-foreground text-sm"
                 >
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-      <SidebarFooter>
+                  {#snippet child({ props })}
+                    <Collapsible.Trigger {...props}>
+                      <group.icon class="mr-2"/>
+                        
+                      {group.title}
+                      <ChevronDown
+                        class="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180"
+                      />
+                    </Collapsible.Trigger>
+                  {/snippet}
+                </Sidebar.GroupLabel>
+                <Collapsible.Content>
+                  <Sidebar.GroupContent>
+                    <Sidebar.Menu>
+                      {#each group.items as item}
+                        <Sidebar.MenuItem>
+                          <Sidebar.MenuButton>
+                            {#snippet child({ props })}
+                              <a href={item.link} {...props}>
+                                <item.icon />
+                                <span>{item.title}</span>
+                              </a>
+                            {/snippet}
+                          </Sidebar.MenuButton>
+                        </Sidebar.MenuItem>
+                      {/each}
+                    </Sidebar.Menu>
+                  </Sidebar.GroupContent>
+                </Collapsible.Content>
+              </Sidebar.Group>
+            </Collapsible.Root>
+          {:else}
+            <Sidebar.Group>
+              <Sidebar.GroupLabel>{group.title}</Sidebar.GroupLabel>
+            </Sidebar.Group>
+          {/if}
+        {/each}
+      </Sidebar.Content>
+      <Sidebar.Footer>
         <ThemeSwitcher />
-      </SidebarFooter>
-    </Sidebar>
-    <SidebarTrigger />
-  </SidebarProvider>
+      </Sidebar.Footer>
+    </Sidebar.Root>
+    <Sidebar.Trigger />
+  </Sidebar.Provider>
 
   <div class="w-full">
     {@render children()}
