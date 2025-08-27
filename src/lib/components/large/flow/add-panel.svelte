095adebfbbff10260b85b86fed4c5c8dd1dd729a
Siddharth <siddharth.060605@gmail.com>
2025-08-27 17:12:16 -0500

    Extract add panel to separate component

---
+++ src/lib/components/large/flow/add-panel.svelte
@@ -0,0 +1,70 @@
+<script>
+    import { Panel } from "@xyflow/svelte";
+    import { Button } from "$lib/components/ui/button";
+    import { triton } from "$lib/api/triton/api";
+
+    import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+    import { Plus } from "@lucide/svelte";
+</script>
+
+<Panel>
+    <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+            {#snippet child({ props })}
+                <Button {...props} variant="outline"><Plus />Add</Button>
+            {/snippet}
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content class="w-56" align="start">
+            <!-- Add an input -->
+            <DropdownMenu.Sub>
+                <DropdownMenu.SubTrigger><Plus />Input</DropdownMenu.SubTrigger>
+                <DropdownMenu.SubContent>
+                    {#await triton.ready then _}
+                        {#each triton.models as model}
+                            <DropdownMenu.Item>{model.name}</DropdownMenu.Item>
+                        {/each}
+                    {/await}
+                </DropdownMenu.SubContent>
+            </DropdownMenu.Sub>
+
+            <!-- Add an output -->
+            <DropdownMenu.Sub>
+                <DropdownMenu.SubTrigger><Plus />Output</DropdownMenu.SubTrigger
+                >
+                <DropdownMenu.SubContent>
+                    {#await triton.ready then _}
+                        {#each triton.models as model}
+                            <DropdownMenu.Item>{model.name}</DropdownMenu.Item>
+                        {/each}
+                    {/await}
+                </DropdownMenu.SubContent>
+            </DropdownMenu.Sub>
+
+            <!-- Add a model -->
+            <DropdownMenu.Sub>
+                <DropdownMenu.SubTrigger><Plus />Model</DropdownMenu.SubTrigger>
+                <DropdownMenu.SubContent>
+                    {#await triton.ready then _}
+                        {#each triton.models as model}
+                            <DropdownMenu.Item>{model.name}</DropdownMenu.Item>
+                        {/each}
+                    {/await}
+                </DropdownMenu.SubContent>
+            </DropdownMenu.Sub>
+
+            <!-- Add a tokenizer -->
+            <DropdownMenu.Sub>
+                <DropdownMenu.SubTrigger
+                    ><Plus />Tokenizer</DropdownMenu.SubTrigger
+                >
+                <DropdownMenu.SubContent>
+                    {#await triton.ready then _}
+                        {#each triton.models as model}
+                            <DropdownMenu.Item>{model.name}</DropdownMenu.Item>
+                        {/each}
+                    {/await}
+                </DropdownMenu.SubContent>
+            </DropdownMenu.Sub>
+        </DropdownMenu.Content>
+    </DropdownMenu.Root>
+</Panel>
--- src/lib/components/large/flow/flow.svelte
+++ src/lib/components/large/flow/flow.svelte
@@ -3,17 +3,13 @@
         Background,
         Controls,
         MiniMap,
-        Panel,
         SvelteFlow,
     } from "@xyflow/svelte";
     import ModelNode from "./model-node.svelte";
     import { mode } from "mode-watcher";
-    import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
-    import { Plus } from "@lucide/svelte";
     import "@xyflow/svelte/dist/style.css";
-    import { Button } from "$lib/components/ui/button";
-    import { triton } from "$lib/api/triton/api";
     import InputNode from "./input-node.svelte";
+    import AddPanel from "./add-panel.svelte";
     let { nodes = $bindable(), edges = $bindable() } = $props();
 </script>
 
@@ -26,80 +22,5 @@
     <Background />
     <MiniMap />
     <Controls />
-    <Panel>
-        <DropdownMenu.Root>
-            <DropdownMenu.Trigger>
-                {#snippet child({ props })}
-                    <Button {...props} variant="outline"
-                        ><Plus />Add</Button
-                    >
-                {/snippet}
-            </DropdownMenu.Trigger>
-            <DropdownMenu.Content class="w-56" align="start">
-                <!-- Add an input -->
-                <DropdownMenu.Sub>
-                    <DropdownMenu.SubTrigger
-                        ><Plus />Add Input</DropdownMenu.SubTrigger
-                    >
-                    <DropdownMenu.SubContent>
-                        {#await triton.ready then _}
-                            {#each triton.models as model}
-                                <DropdownMenu.Item
-                                    >{model.name}</DropdownMenu.Item
-                                >
-                            {/each}
-                        {/await}
-                    </DropdownMenu.SubContent>
-                </DropdownMenu.Sub>
-
-                <!-- Add an output -->
-                <DropdownMenu.Sub>
-                    <DropdownMenu.SubTrigger
-                        ><Plus />Add Output</DropdownMenu.SubTrigger
-                    >
-                    <DropdownMenu.SubContent>
-                        {#await triton.ready then _}
-                            {#each triton.models as model}
-                                <DropdownMenu.Item
-                                    >{model.name}</DropdownMenu.Item
-                                >
-                            {/each}
-                        {/await}
-                    </DropdownMenu.SubContent>
-                </DropdownMenu.Sub>
-
-                <!-- Add a model -->
-                <DropdownMenu.Sub>
-                    <DropdownMenu.SubTrigger
-                        ><Plus />Add Model</DropdownMenu.SubTrigger
-                    >
-                    <DropdownMenu.SubContent>
-                        {#await triton.ready then _}
-                            {#each triton.models as model}
-                                <DropdownMenu.Item
-                                    >{model.name}</DropdownMenu.Item
-                                >
-                            {/each}
-                        {/await}
-                    </DropdownMenu.SubContent>
-                </DropdownMenu.Sub>
-
-                <!-- Add a tokenizer -->
-                <DropdownMenu.Sub>
-                    <DropdownMenu.SubTrigger
-                        ><Plus />Add a Tokenizer</DropdownMenu.SubTrigger
-                    >
-                    <DropdownMenu.SubContent>
-                        {#await triton.ready then _}
-                            {#each triton.models as model}
-                                <DropdownMenu.Item
-                                    >{model.name}</DropdownMenu.Item
-                                >
-                            {/each}
-                        {/await}
-                    </DropdownMenu.SubContent>
-                </DropdownMenu.Sub>
-            </DropdownMenu.Content>
-        </DropdownMenu.Root>
-    </Panel>
+    <AddPanel/>
 </SvelteFlow>
